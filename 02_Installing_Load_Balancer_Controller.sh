785be8ec29ce745d13e80647d6e86fcb3495e04b
Mahmood Rohani <mahmoodrm@gmail.com>
2024-03-12 16:43:14 +0330

    Update 02_Installing_Load_Balancer_Controller.sh

--- 02_Installing_Load_Balancer_Controller.sh
+++ 02_Installing_Load_Balancer_Controller.sh
@@ -10,3 +10,8 @@ aws cloudformation deploy \
     --stack-name aws-load-balancer-iam-policy \
     --template-file iam-policy.yaml \
     --capabilities CAPABILITY_IAM
+
+
+aws iam attach-role-policy \
+--role-name $(aws cloudformation list-exports --query "Exports[?contains(Name, 'nodegroup-eks-node-group::InstanceRoleARN')].Value" --output text | xargs | cut -d "/" -f 2) 
+--policy-arn $(aws cloudformation describe-stacks --stack-name aws-load-balancer-iam-policy --query "Stacks[*].Outputs[?OutputKey=='IamPolicyArn'].OutputValue" --output text | xargs)
